48c19bfbe31b699bcb6a681b2a1f86ce493fad9e
raialmeida <raialmeida94_@hotmail.com>
2024-04-24 15:34:05 -0300

    json object adjustments

--- src/test/java/Services/CadastroProdutos/payloads/CadastroProdutoPayloads.java
+++ src/test/java/Services/CadastroProdutos/payloads/CadastroProdutoPayloads.java
@@ -8,20 +8,19 @@ import jakarta.json.JsonObjectBuilder;
 public class CadastroProdutoPayloads {
 
 	public static String payloadCadastroProduto(String produto, Integer preco) {
-
 		JsonObjectBuilder builder = Json.createObjectBuilder()
 				.add("nome", produto)
 				.add("descricao", "Mouse")
 				.add("quantidade", "381");
 
 		if (preco != null) {
-			builder.add(preco != null ? "preco" : "", preco);
+			builder.add("preco", preco);
 		}
+
 		return builder.build().toString();
 	}
 
 	public static String payloadCadastroProduto() {
-
 		String produtoFaker = Faker.instance().commerce().productName();
 		return payloadCadastroProduto(produtoFaker, 470);
 	}
